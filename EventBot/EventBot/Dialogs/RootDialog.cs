40a38c71a972febdc8921bf73f22a788f5d79cec
daheinze <daheinze@JARVIS_WORK>
2017-06-21 16:50:10 +0200

    Included output

--- EventBot/EventBot/Dialogs/RootDialog.cs
+++ EventBot/EventBot/Dialogs/RootDialog.cs
@@ -2,12 +2,19 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using EventBot.API;
+using EventBot.Model;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EventBot.Dialogs
 {
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+        private LuisResult luisResult;
+        private const string RETURN_GREETING = "Do you want to ask another question? If you do, go ahead.";
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -17,15 +24,133 @@ namespace EventBot.Dialogs
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
+            var greeting = "Hi, I'm Eve, your event bot. You can ask me questions about your event. Please state a question.";
+
+            await context.PostAsync(greeting);
+
+            context.Wait(WaitForLuisMessage);
+        }
+
+        private async Task WaitForLuisMessage(IDialogContext context, IAwaitable<object> result)
+        {
             var activity = await result as Activity;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            var TextToLuis = activity.Text;
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            luisResult = await LuisConnector.GetLuisResult(TextToLuis);
 
-            context.Wait(MessageReceivedAsync);
+            await this.HandleLuisMessage(context);
         }
+
+        private async Task HandleLuisMessage(IDialogContext context)
+        {
+            List<EventSpeaker> allEventInfos = SqlConnector.GetEventSpeakerInfo();
+
+            var entity = (from l in luisResult.entities where l.type == "speaker" select l).FirstOrDefault();
+
+            switch (luisResult.topScoringIntent.intent)
+            {
+                case "talk.speaker":
+                    {
+                        if (entity != null)
+                        {
+                            var speakerInfo = allEventInfos.Where(x => x.SpeakerName == entity.entity).FirstOrDefault();
+
+                            // Filter for Session time
+                            var resultText = $"The next session from {entity.entity} is at {speakerInfo.TalkTime.Hour}:{speakerInfo.TalkTime.Minute}.";
+
+                            await context.PostAsync(resultText);
+                            await context.PostAsync(RETURN_GREETING);
+                            context.Wait(WaitForLuisMessage);
+                        }
+                        else
+                        {
+                            // Ask again
+                            await context.PostAsync("I couldn't find the speaker you are looking for. Please include the speaker in your request and try again.");
+                            context.Wait(WaitForLuisMessage);
+                        }
+                    }
+                    break;
+                case "talk.content":
+                    {
+                        if (entity != null)
+                        {
+                            var speakerInfo = allEventInfos.Where(x => x.SpeakerName == entity.entity).FirstOrDefault();
+
+                            // Filter for Speaker Content
+                            var resultText = $"{entity.entity} talks about the following topic: {speakerInfo.TalkDescription}";
+
+                            await context.PostAsync(resultText);
+                            await context.PostAsync(RETURN_GREETING);
+                            context.Wait(WaitForLuisMessage);
+                        }
+                        else
+                        {
+                            // Ask again
+                            await context.PostAsync("I couldn't find the speaker you are looking for. Please include the speaker in your request and try again.");
+                            context.Wait(WaitForLuisMessage);
+                        }
+                    }
+                    break;
+                case "talk.next":
+                    {
+                        // Get current time
+                        var currentTime = DateTime.Now;
+
+                        // Filter for time
+                        var allLaterSpeakers = allEventInfos.Where(x => x.TalkTime.CompareTo(currentTime) > 0).ToList();
+
+                        if (allLaterSpeakers.Count == 0)
+                        {
+                            await context.PostAsync("There are no sessions left. Happy hacking :-)");
+                            await this.StartAsync(context);
+                        }
+                        else
+                        {
+                            await DisplaySpeakerInformation(context, allLaterSpeakers);
+                        }
+                        
+                        
+
+                    }
+                    break;
+                default:
+                    {
+                        await context.PostAsync("I couldn't find the speaker you are looking for. Please include the speaker in your request and try again.");
+                        context.Wait(WaitForLuisMessage);
+                    }
+                    break;
+            }
+        }
+
+        private async Task DisplaySpeakerInformation(IDialogContext context, List<EventSpeaker> allLaterSpeakers)
+        {
+            var finalMessage = context.MakeMessage();
+            finalMessage.Recipient = context.MakeMessage().From;
+            finalMessage.Type = "message";
+            finalMessage.Attachments = new List<Attachment>();
+            finalMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            finalMessage.TextFormat = TextFormatTypes.Markdown;
+
+            foreach (var speaker in allLaterSpeakers)
+            {
+                HeroCard resultCard = new HeroCard()
+                {
+                    Images = new List<CardImage> { new CardImage(speaker.SpeakerImageUrl) },
+                    Title = speaker.SpeakerName,
+                    Text = speaker.TalkDescription.Substring(0, 150),
+                    Subtitle = String.Format("{0:t}", speaker.TalkTime)
+            };
+
+                Attachment cardAttachment = resultCard.ToAttachment();
+                finalMessage.Attachments.Add(cardAttachment);
+            }
+
+            await context.PostAsync(finalMessage);
+            await context.PostAsync(RETURN_GREETING);
+            context.Wait(WaitForLuisMessage);
+
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -29,16 +29,15 @@ Fill the database with the information needed for the event bot. To do this, use
 
 After the data is included in the database we have to figure out, what kinds of questions the user wants to ask the bot. In this simple scenario we want to get answers to the following questions:
 
-1. When is the next talk?
-2. What is \<speaker> talking about?
-3. When is the session from \<speaker>?
+1. What can I do next?
+1. What is \<speaker> talking about?
+1. When is the session from \<speaker>?
 
 These questions will be mapped to 3 **intents**:
 
+1. talk.next
+1. talk.content
 1. talk.speaker
-2. talk.content
-3. talk.time
-
 
 The value of \<speaker> is stored in an **entity** called: **speakerName**
 
@@ -50,15 +49,15 @@ We train each intent with 5-10 phrases that we expect the user to say, when he w
 
 To train and publish you LUIS model, follow the steps as described:
 
-1. Create a new app on [luis.ai](https://www.luis.ai/applications)
+1. **Create a new app on [luis.ai]**(https://www.luis.ai/applications)
 
     ![Create App](images/1_Create_App.png)
 
-1. Go to intents and create a new intent
+1. **Go to intents and create a new intent**
 
     ![Create App](images/2_Intent.png)
 
-1. Add phrases to the intent
+1. **Add phrases to the intent**
 
     Add the phrases the service should understand, but don't worry, because LUIS is an intelligent service, it can adapt to changes in the query, so if a word is missing or added or the word order is changed, it will still trigger the intent as intended - or should I say: **intented** :laughing:
 
@@ -66,21 +65,25 @@ To train and publish you LUIS model, follow the steps as described:
 
     <img src="https://media.giphy.com/media/d7fTn7iSd2ivS/giphy.gif" width=400 />
 
-1. Create a new entity to recognize properties
+1. **Create a new entity to recognize properties**
 
     To add a entity, select the values that will be dynamic by clicking on them. Type a name and click "Create entity".
 
     ![Create App](images/3_Add_Entity.png)
 
-1. Training the service
+1. **Training the service**
 
     ![Create App](images/4_Train_Test.png)
 
-1. Publish the service
+1. **Publish the service**
 
     ![Create App](images/5_Publish.png)
 
 
-# Bot development
+# Bot development #
 
+## Integrating LUIS ##
 
+## Integrating the database ##
+
+## Adding speech capabilities ##
